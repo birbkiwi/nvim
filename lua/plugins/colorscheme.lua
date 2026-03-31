c4965165306fe0ba07e5b6945f08e4376d9f37e1
Mustafa <Mustafa.elkalyoubi.e@thalesdigital.io>
2026-03-31 12:01:59 +0400

    add colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,58 +1,32 @@
 return {
-  { 'folke/tokyonight.nvim', enabled = false },
+  { 'folke/tokyonight.nvim', lazy = true },
   {
     'rebelot/kanagawa.nvim',
     opts = {
-        compile = true, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = true, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors) -- add/modify highlights
-          return {}
-        end,
-        theme = 'wave',
-        background = {
-          dark = 'wave',
-          light = 'lotus',
-        },
+      compile = true, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = true, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
       },
-    -- config = function()
-    --   require('kanagawa').setup {
-    --     compile = false, -- enable compiling the colorscheme
-    --     undercurl = true, -- enable undercurls
-    --     commentStyle = { italic = true },
-    --     functionStyle = {},
-    --     keywordStyle = { italic = true },
-    --     statementStyle = { bold = true },
-    --     typeStyle = {},
-    --     transparent = true, -- do not set background color
-    --     dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-    --     terminalColors = true, -- define vim.g.terminal_color_{0,17}
-    --     colors = { -- add/modify theme and palette colors
-    --       palette = {},
-    --       theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-    --     },
-    --     overrides = function(colors) -- add/modify highlights
-    --       return {}
-    --     end,
-    --     theme = 'wave',
-    --     background = {
-    --       dark = 'wave',
-    --       light = 'lotus',
-    --     },
-    --   }
-    --   -- vim.cmd 'colorscheme kanagawa',
-    -- end,
+      theme = 'wave',
+      background = {
+        dark = 'wave',
+        light = 'lotus',
+      },
+    },
+  },
+  {
+    'loctvl842/monokai-pro.nvim',
+    lazy = true,
   },
 
   {
